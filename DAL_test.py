10c37410fc6a91b7d81c4da037a90508c83eff76
emilysilcock <emilysilcock@gmail.com>
2024-03-04 10:34:55 -0500

    Make binary

--- DAL_test.py
+++ DAL_test.py
@@ -1,40 +1,70 @@
+import datasets
+import logging
+import torch
+import numpy as np
+from transformers import AutoTokenizer
+from small_text import (
+    TransformersDataset,
+    PoolBasedActiveLearner,
+    PredictionEntropy,
+    DiscriminativeActiveLearning,
+    TransformerBasedClassificationFactory,
+    TransformerModelArguments,
+    random_initialization_balanced
+)
+from sklearn.metrics import accuracy_score, f1_score
+
+
+
+def make_binary(dataset, target_labels):
+
+    # Create mapping
+    num_classes = dataset.features['label'].num_classes
+
+    class_mapping = {lab: 0 for lab in range(num_classes)}
+    
+    for tl in target_labels:
+        class_mapping[tl] = 1
+
+    # Apply the mapping to change the labels
+    binary_dataset = dataset.map(lambda example: {'label': class_mapping[example['label']]})
+
+    # Update metadata
+    new_features = datasets.Features({
+        'text': binary_dataset.features['text'],
+        'label': datasets.ClassLabel(names = ['merged', 'target'], num_classes=2)
+        })
+    binary_dataset = binary_dataset.cast(new_features)
+
+    return binary_dataset
+
 
 ### Preparation
-import datasets
 datasets.logging.set_verbosity_error()
 
 # disables the progress bar for notebooks: https://github.com/huggingface/datasets/issues/2651
 datasets.logging.get_verbosity = lambda: logging.NOTSET
-
-
-import torch
-import numpy as np
 
 seed = 2022
 torch.manual_seed(seed)
 np.random.seed(seed)
 
 ## II. Data
-import logging
-
 raw_dataset = datasets.load_dataset('ag_news')
+
+### Make binary 
+raw_dataset['train'] = make_binary(raw_dataset['train'], target_labels=[0])
+raw_dataset['test'] = make_binary(raw_dataset['test'], target_labels=[0])
+
+
 num_classes = raw_dataset['train'].features['label'].num_classes
 
-print('First 10 training samples:\n')
-for i in range(10):
-    print(raw_dataset['train']['label'][i], ' ', raw_dataset['train']['text'][i])
-
 ### Preparing the Data
-from transformers import AutoTokenizer
-
 transformer_model_name = 'bert-base-uncased'
 
 tokenizer = AutoTokenizer.from_pretrained(
     transformer_model_name
 )
-
-from small_text import TransformersDataset
-
 
 target_labels = np.arange(num_classes)
 
@@ -43,23 +73,13 @@ train = TransformersDataset.from_arrays(raw_dataset['train']['text'],
                                         tokenizer,
                                         max_length=60,
                                         target_labels=target_labels)
-test = TransformersDataset.from_arrays(raw_dataset['test']['text'], 
+test = TransformersDataset.from_arrays(raw_dataset['test']['text'],
                                        raw_dataset['test']['label'],
                                        tokenizer,
                                        max_length=60,
                                        target_labels=target_labels)
 
 ## III. Setting up the Active Learner
-
-import small_text
-from small_text import (
-    PoolBasedActiveLearner,
-    PredictionEntropy,
-    DiscriminativeActiveLearning,
-    TransformerBasedClassificationFactory,
-    TransformerModelArguments,
-    random_initialization_balanced
-)
 
 
 # simulates an initial labeling to warm-start the active learning process
@@ -72,20 +92,19 @@ def initialize_active_learner(active_learner, y_train):
 
 
 transformer_model = TransformerModelArguments(transformer_model_name)
-clf_factory = TransformerBasedClassificationFactory(transformer_model, 
-                                                    num_classes, 
-                                                    kwargs=dict({'device': 'cuda', 
+clf_factory = TransformerBasedClassificationFactory(transformer_model,
+                                                    num_classes,
+                                                    kwargs=dict({'device': 'cuda',
                                                                  'mini_batch_size': 32,
                                                                  'class_weight': 'balanced'
                                                                 }))
 
-clf_factory_2 = TransformerBasedClassificationFactory(transformer_model, 
-                                                    num_classes, 
-                                                    kwargs=dict({'device': 'cuda', 
+clf_factory_2 = TransformerBasedClassificationFactory(transformer_model,
+                                                    num_classes,
+                                                    kwargs=dict({'device': 'cuda',
                                                                  'mini_batch_size': 32,
                                                                  'class_weight': 'balanced'
                                                                 }))
-
 
 # query_strategy = PredictionEntropy()
 query_strategy = DiscriminativeActiveLearning(classifier_factory=clf_factory_2, num_iterations=10)
@@ -94,30 +113,27 @@ active_learner = PoolBasedActiveLearner(clf_factory, query_strategy, train)
 indices_labeled = initialize_active_learner(active_learner, train.y)
 
 ### Active Learning Loop
-from sklearn.metrics import accuracy_score, f1_score
-
-
 num_queries = 10
 
 
 def evaluate(active_learner, train, test):
     y_pred = active_learner.classifier.predict(train)
     y_pred_test = active_learner.classifier.predict(test)
-    
+
     test_acc = accuracy_score(y_pred_test, test.y)
 
     print('Train accuracy: {:.2f}'.format(accuracy_score(y_pred, train.y)))
     print('Test accuracy: {:.2f}'.format(test_acc))
     print('Train F1: {:.2f}'.format(f1_score(y_pred, train.y, average="weighted")))      ############################
     print('Test F1: {:.2f}'.format(f1_score(y_pred_test, test.y, average="weighted")))   ############################
-    
+
     return test_acc
 
 
 results = []
 results.append(evaluate(active_learner, train[indices_labeled], test))
 
-    
+
 for i in range(num_queries):
     # ...where each iteration consists of labelling 20 samples
     indices_queried = active_learner.query(num_samples=20)
@@ -129,7 +145,7 @@ for i in range(num_queries):
     active_learner.update(y)
 
     indices_labeled = np.concatenate([indices_queried, indices_labeled])
-    
+
     print('---------------')
     print(f'Iteration #{i} ({len(indices_labeled)} samples)')
     results.append(evaluate(active_learner, train[indices_labeled], test))
