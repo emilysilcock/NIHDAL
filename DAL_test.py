7d953df16926d21a5c188f3df28bd6c6dd2b6414
emilysilcock <emilysilcock@gmail.com>
2024-03-12 22:42:52 -0400

    test

--- DAL_test.py
+++ DAL_test.py
@@ -588,8 +588,8 @@ if __name__ == '__main__':
     biased = True
     transformer_model_name = 'distilroberta-base'
 
-    for als in ['NIHDAL_simon', 'NIHDAL']:
-    # for als in ['Random']:
+    # for als in ['NIHDAL_simon', 'NIHDAL']:
+    for als in ['Random']:
 
         # Set seed
         for seed in [12731, 65372]:  #42
