1f8804da742be07c550aaecfa0009bbe0be4b373
emilysilcock <emilysilcock@gmail.com>
2024-03-16 08:38:13 -0400

    run others

--- active_learning_experiments.py
+++ active_learning_experiments.py
@@ -586,12 +586,13 @@ if __name__ == '__main__':
         transformer_model_name = 'distilroberta-base'
 
         # for als in ["Random", "Least Confidence", "BALD", "BADGE", "DAL", "Core Set", "Contrastive", 'NIHDAL', 'NIHDAL_simon']:
-        for als in ['NIHDAL']:
+        for als in ["Random", "Least Confidence", "BALD", "BADGE", "DAL", "Core Set", "Contrastive"]:
+        # for als in ['NIHDAL']:
 
             print(f'****************{als}**********************')
 
             # Set seed
-            for seed in [65372, 97, 163]:  # 42 # 12731, 
+            for seed in [42, 12731, 65372, 97, 163]:  # 42 # 12731,
 
                 print(f'#################{seed}##################')
                 torch.manual_seed(seed)
