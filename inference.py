475eb7082c93d86c14123c9fc8b12adf19c5beec
emilysilcock <emilysilcock@gmail.com>
2024-04-03 19:22:28 -0400

    test

--- inference.py
+++ inference.py
@@ -42,7 +42,8 @@ def basic_clean(fp, first_date, sp):
 
             content = art['Document']['Content']
 
-            soup = BeautifulSoup(content, 'xml')
+            # soup = BeautifulSoup(content, 'xml')
+            soup = BeautifulSoup(content, "html5lib")
 
             # Get Date
             date = datetime.strptime(art["Date"], "%Y-%m-%dT%H:%M:%SZ").date()
