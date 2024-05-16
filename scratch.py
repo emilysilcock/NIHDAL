cf85091d5af3c0c1e55152403514d980e7d67e22
emilysilcock <emilysilcock@gmail.com>
2024-05-16 18:11:08 -0400

    test

---
+++ scratch.py
@@ -0,0 +1,20 @@
+
+from tqdm import tqdm
+import json
+
+if __name__ == '__main__':
+
+
+    for num in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    
+        print(f'**{num}**')
+
+        # Format and tokenize
+        with open(f"/mnt/data01/AL/clean_data/'The_Sun_(England)'/group_{num}/cleaned_sample_data_earlier.json") as f:
+            data = json.load(f)
+
+        for art_id, art_dict in tqdm(data.items()):
+
+            if art_dict["headline"] == "OAP A RAPIST":
+
+                print(f'********************{num}*************************')
