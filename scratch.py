25bd671e67a4d657f0f3e53a4eebed221fb5d234
emilysilcock <emilysilcock@gmail.com>
2024-05-16 18:12:28 -0400

    test

--- scratch.py
+++ scratch.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         print(f'**{num}**')
 
         # Format and tokenize
-        with open(f"/mnt/data01/AL/clean_data/'The_Sun_(England)'/group_{num}/cleaned_sample_data_earlier.json") as f:
+        with open(f"/mnt/data01/AL/clean_data/'The_Sun_(England)'/group_{num}/cleaned_sample_data.json") as f:
             data = json.load(f)
 
         for art_id, art_dict in tqdm(data.items()):
