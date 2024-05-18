a18efcde36ecd214728dcd1afdd8db839b7b2cdb
emilysilcock <emilysilcock@gmail.com>
2024-05-18 11:04:41 -0400

    test

--- scratch.py
+++ scratch.py
@@ -5,7 +5,7 @@ import json
 if __name__ == '__main__':
 
 
-    for num in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    for num in [3]:
     
         print(f'**{num}**')
 
@@ -13,9 +13,11 @@ if __name__ == '__main__':
         with open(f"/mnt/data01/AL/clean_data/'The_Sun_(England)'/group_{num}/cleaned_sample_data.json") as f:
             data = json.load(f)
 
-        for art_id, art_dict in tqdm(data.items()):
+        for art_id, art_dict in data.items():
 
             if "....and this is where stands" in art_dict["headline"].lower():
 
-                print(f'********************{num}*************************')
-                print(art_dict["headline"])
+                example = art_dict
+
+    print(json.dumps(example, indent=3))
+
