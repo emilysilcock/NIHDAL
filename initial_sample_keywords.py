e0dbc63aa7764a06b081189cc394124d4858d709
emilysilcock <emilysilcock@gmail.com>
2024-05-18 19:20:54 -0400

    neaten

--- initial_sample_keywords.py
+++ initial_sample_keywords.py
@@ -4,55 +4,23 @@ import json
 import re
 import random
 
+from data_fns import get_pub_list
+
 if __name__ == '__main__':
 
 
-    publications = [
-        'The Sun (England)',
-        'thesun.co.uk',
-        'Daily Star',
-        'Daily Star Online',
-        'Daily Star Sunday',
-        'The Daily Mail and Mail on Sunday (London)',
-        'mirror.co.uk',
-        'Daily Mirror',
-        'The Express',
-        'The Sunday Express',
-        'The News of the World',
-        'The Evening Standard (London)',
-        'standard.co.uk',
-        'The People',
-        'Metro (UK)',
-        'City A.M.',
-        'Cityam.com',
-        'The Times (London)',
-        'The Sunday Times (London)',
-        'thetimes.co.uk',
-        'The Daily Telegraph (London)',
-        'The Daily Telegraph Online',
-        'The Sunday Telegraph (London)',
-        'The Guardian (London)',
-        'The Observer (London)',
-        'i - Independent Print Ltd',
-        'The Independent (United Kingdom)',
-        'Liverpool Post',
-        'liverpoolecho.co.uk',
-        'Liverpool Echo',
-    ]
+    publications = get_pub_list()
 
     keyword_list_1 = ['social security', 'benefit fraud', 'scrounger', 'shirker', 'sponger',
                     'skiver', 'workshy', 'work-shy', 'something for nothing', 'underclass', 'benefit tourism', 'benefit tourist']
-
     keyword_list_2 = ['dole']
-
     keyword_list_3 = ['benefits', 'welfare']
-
 
     sample_size = 10
 
     selected_articles = []
 
-    # Open all data   
+    # Open all data
     for publication in tqdm(publications):
         sample_list = []
 
