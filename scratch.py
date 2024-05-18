c1de029d4e2af9bdde7de692804b4c616c59671d
emilysilcock <emilysilcock@gmail.com>
2024-05-18 13:46:40 -0400

    test

--- scratch.py
+++ scratch.py
@@ -5,24 +5,56 @@ import json
 if __name__ == '__main__':
 
 
-    for num in [3]:
+    publications = [
+        'The Sun (England)',
+        'thesun.co.uk',
+        'Daily Star',
+        'Daily Star Online',
+        'Daily Star Sunday',
+        'The Daily Mail and Mail on Sunday (London)',
+        'mirror.co.uk',
+        'Daily Mirror',
+        'The Express',
+        'The Sunday Express',
+        'The News of the World',
+        'The Evening Standard (London)',
+        'standard.co.uk',
+        'The People',
+        'Metro (UK)',
+        'City A.M.',
+        'Cityam.com',
+        'The Times (London)',
+        'The Sunday Times (London)',
+        'thetimes.co.uk',
+        'The Daily Telegraph (London)',
+        'The Daily Telegraph Online',
+        'The Sunday Telegraph (London)',
+        'The Guardian (London)',
+        'The Observer (London)',
+        'i - Independent Print Ltd',
+        'The Independent (United Kingdom)',
+        'Liverpool Post',
+        'liverpoolecho.co.uk',
+        'Liverpool Echo',
+    ]
+
+    # Open all data
+    scrounger_list = []
+
+    for publication in tqdm(publications):
+
+        publication_fn = publication.replace(' ', '_')
+
+        with open(f"Sun_data/{publication_fn}/cleaned_sample_data.json") as f:
+            clean_dat = json.load(f)
+
+        for art_id, art_dict in clean_dat.items():
+            if "scrounger" in art_dict['article'].lower() or "scrouner" in art_dict["headline"].lower():
+                scrounger_list.append(art_dict)
+
+    print(len(art_dict))
+
+
+        
+
     
-        print(f'**{num}**')
-
-        # Format and tokenize
-        with open(f"/mnt/data01/AL/clean_data/'The_Sun_(England)'/group_{num}/cleaned_sample_data.json") as f:
-            data = json.load(f)
-
-        for art_id, art_dict in data.items():
-
-            if "....and this is where stands" in art_dict["headline"].lower():
-
-                example = art_dict
-                print(art_id)
-                print(type(art_id))
-
-    print(json.dumps(example, indent=3))
-
-    
-
-
