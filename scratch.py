3944215b0f8462f9309a3e33174df22d61886377
emilysilcock <emilysilcock@gmail.com>
2024-05-22 21:58:26 -0400

    debug

--- scratch.py
+++ scratch.py
@@ -6,18 +6,13 @@ with open('Labelled_data/kw_initialisation/sample_1.json') as f:
 
 new_labelled_data = []
 
-counter = 0
-for i, art in enumerate(dat):
+for art in enumerate(dat):
 
-    if i != 0:
-        if art['data']['ln_id'] == dat[i-1]['data']['ln_id']:
-            counter += 1
-        else:
-            counter = 0
+    chunk_id = art['data']['chunks'].index(art['data']['article'])
 
     art_copy = copy.deepcopy(art)
 
-    art_copy['data']['ln_id'] = f"{art_copy['data']['ln_id']}_{counter}"
+    art_copy['data']['ln_id'] = f"{art_copy['data']['ln_id']}_{chunk_id}"
 
     new_labelled_data.append(art_copy)
 
