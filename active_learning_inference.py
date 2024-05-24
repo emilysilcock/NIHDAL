b7cf39a92ac949b2763cf14d96943a7b895ee7fc
emilysilcock <emilysilcock@gmail.com>
2024-05-24 11:26:52 -0400

    Add path

--- active_learning_inference.py
+++ active_learning_inference.py
@@ -316,7 +316,8 @@ if __name__ == '__main__':
 
     sample_list = open_pool('Sun_data/chunked_sample.json')
     parsed_labelled_data = open_labelled_data([
-        'Labelled_data/kw_initialisation/sample_1_with_correct_ids.json'
+        'Labelled_data/kw_initialisation/sample_1_with_correct_ids.json',
+        'Labelled_data/kw_initialisation/sample_2.json',
         ])
 
     texts = []
@@ -381,5 +382,5 @@ if __name__ == '__main__':
             "data": sample_list[i]
         })
 
-    with open(f'data_to_label/kw_initialisation/sample_2.json', 'w') as f:
+    with open(f'data_to_label/kw_initialisation/sample_3.json', 'w') as f:
         json.dump(to_label, f, indent=2)
