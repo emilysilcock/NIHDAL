bc22374424b41f26ca08372856e73bdb0b9773a0
emilysilcock <emilysilcock@gmail.com>
2024-05-24 09:42:38 -0400

    Switch filepaths

--- carry_across_labels.py
+++ carry_across_labels.py
@@ -1,13 +1,12 @@
 import json
 
 # Get labels from old data
-# old_labelled_data_paths = ['Labelled_data/fixed_first_1000.json',
-#                   'Labelled_data/sample_11_fixed.json',
-#                   'Labelled_data/sample_12_fixed.json',
-#                   'Labelled_data/sample_13_fixed.json',
-#                   'Labelled_data/sample_14_fixed.json',
-#                   'Labelled_data/sample_15_fixed.json']
-old_labelled_data_paths = ['Labelled_data/kw_initialisation/sample_1.json']
+old_labelled_data_paths = ['Labelled_data/fixed_first_1000.json',
+                  'Labelled_data/sample_11_fixed.json',
+                  'Labelled_data/sample_12_fixed.json',
+                  'Labelled_data/sample_13_fixed.json',
+                  'Labelled_data/sample_14_fixed.json',
+                  'Labelled_data/sample_15_fixed.json']
 
 
 old_labelled_data = {}
@@ -21,7 +20,7 @@ for p in old_labelled_data_paths:
 
 
 # Add labels to new data 
-name_bit = "first_sample_with_correct_ids"
+name_bit = "sample_2"
 
 with open(f'data_to_label/kw_initialisation/{name_bit}.json') as f:
     new_dat = json.load(f)
@@ -68,8 +67,5 @@ for f in new_dat:
 
 print(f'{count} already labelled')
 
-# with open(f'data_to_label/kw_initialisation/{name_bit}_with_old_labels.json', 'w') as f:
-#     json.dump(new_dat, f, indent=4)
-
-with open('Labelled_data/kw_initialisation/sample_1_with_correct_ids.json', 'w') as f:
+with open(f'data_to_label/kw_initialisation/{name_bit}_with_old_labels.json', 'w') as f:
     json.dump(new_dat, f, indent=4)
