fc4c1bc415d446f0db1705e709dba7d9c6a622ae
emilysilcock <emilysilcock@gmail.com>
2024-06-07 11:56:41 -0400

    paths

--- active_learning_inference.py
+++ active_learning_inference.py
@@ -343,6 +343,7 @@ if __name__ == '__main__':
         'Labelled_data/kw_initialisation/sample_22.json',
         'Labelled_data/kw_initialisation/sample_23.json',
         'Labelled_data/kw_initialisation/sample_24.json',
+        'Labelled_data/kw_initialisation/sample_25.json',
         ])
 
     texts = []
@@ -407,5 +408,5 @@ if __name__ == '__main__':
             "data": sample_list[i]
         })
 
-    with open(f'data_to_label/kw_initialisation/sample_25.json', 'w') as f:
+    with open(f'data_to_label/kw_initialisation/sample_26.json', 'w') as f:
         json.dump(to_label, f, indent=2)
