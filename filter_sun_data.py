b77ca2c3267adaa1c0cc70428ead7464db452c72
emilysilcock <emilysilcock@gmail.com>
2024-06-13 19:04:00 -0400

    Add cleaning

--- filter_sun_data.py
+++ filter_sun_data.py
@@ -4,6 +4,9 @@ from tqdm import tqdm
 import json
 
 
+from data_fns import basic_parsing
+
+
 def date_range(start_date, end_date):
 
     start_date = datetime.strptime(start_date, "%Y-%m-%d")
@@ -20,22 +23,50 @@ def date_range(start_date, end_date):
     return dates
 
 
+edition_dict = {
 
-date_list = date_range(start_date="2013-01-01", end_date="2023-12-31")
+}
 
-for date in tqdm(date_list):
+for year in range(2013, 2023):
 
-    paths = glob(f'/n/home09/esilcock/Sun_data/**{date}**')
+    year_list = []
+    not_found_list = []
 
-    date_data = []
-    for path in paths:
+    print(f'************************{year}***********************')
 
-        with open(path) as f:
-            dat = json.load(f)
+    date_list = date_range(start_date=f"{year}-01-01", end_date=f"{year}-12-31")
 
-        date_data.extend([d["ResultId"] for d in dat['value']])
+    for date in tqdm(date_list):
 
-    date_data = list(set(date_data))
+        paths = glob(f'/n/home09/esilcock/Sun_data/**{date}**')
 
-    assert len(date_data) == dat["@odata.count"]
-        
+        date_ids = []
+        date_data = []
+        for path in paths:
+
+            with open(path) as f:
+                dat = json.load(f)
+
+            date_ids.extend([d["ResultId"] for d in dat['value']])
+            date_data.extend(dat['value'])
+
+        date_ids = list(set(date_ids))
+
+        assert len(date_ids) == dat["@odata.count"]
+
+        cleaned_data, not_found_data = basic_parsing(date_data)
+        not_found_list.append(not_found_data)
+
+        national_editions = []
+
+        for art in cleaned_data:
+            if edition_dict[art['edition']] == 'keep':
+                year_list.append(art)
+
+    with open(f'/n/home09/esilcock/clean_Sun_data/{year}_cleaned.json', 'w') as f:
+        json.dumps(year_list, f, indent=4)
+    with open(f'/n/home09/esilcock/clean_Sun_data/{year}_not_found.json', 'w') as f:
+        json.dumps(not_found_list, f, indent=4)
+
+    print('Filtered articles:', len(year_list))
+    print('Not found:', len(not_found_list))
