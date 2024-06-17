84ba638ccfd4ab789995e3d777ac25cd39d397f8
emilysilcock <emilysilcock@gmail.com>
2024-06-17 10:20:37 +0100

    test

--- clean_not_found.py
+++ clean_not_found.py
@@ -1,5 +1,7 @@
 import json
 
+all_not_found = []
+
 for year in range(2013, 2023):
 
     print(year)
@@ -7,4 +9,8 @@ for year in range(2013, 2023):
     with open(f'/n/home09/esilcock/clean_Sun_data/{year}_not_found.json') as f:
         not_found_list = json.load(f)
 
-    print(sum([len(nf) for nf in not_found_list]))
+    for nf_list in not_found_list:
+        all_not_found.extend(nf_list)
+
+    print(len(all_not_found))
+
