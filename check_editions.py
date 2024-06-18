307ade8e90d853ac4f59de8cdc284440bd595664
emilysilcock <emilysilcock@gmail.com>
2024-06-18 14:17:24 +0100

    debug

--- check_editions.py
+++ check_editions.py
@@ -10,7 +10,7 @@ for year in range(2013, 2023):
     with open(f'/n/home09/esilcock/mentions_benefits/mentions_benefits_{year}.json') as f:
         mentions_benefits = json.load(f)
 
-    for art in tqdm(mentions_benefits):
+    for art_ids, art in tqdm(mentions_benefits.items()):
         if art['edition'] not in editions:
             editions.append(art['edition'])
 
