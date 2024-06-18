ff2d93908856b40255c3c265e940417ce8733a01
emilysilcock <emilysilcock@gmail.com>
2024-06-18 15:32:07 +0100

    test

--- mentions_benefits_inference.py
+++ mentions_benefits_inference.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
 
     # Open data 
     # for year in range(2013, 2023):
-    for year in [2019]:
+    for year in [2013]:
 
         print(f"******************{year}**********************")
 
@@ -83,7 +83,7 @@ if __name__ == '__main__':
             year_dat = json.load(f)
 
         #########
-        year_dat = [a for a in year_dat if "worst benefits cheat" in a['headline'].lower()]
+        year_dat = [a for a in year_dat if "scroungers like barry rogerson make" in a['headline'].lower()]
         print(len(year_dat))
         print("??????????????????????????")
         #########
