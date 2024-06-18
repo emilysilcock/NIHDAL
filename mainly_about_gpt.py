708418ca98cbd936577119c80bc1a1a3682e47eb
emilysilcock <emilysilcock@gmail.com>
2024-06-18 14:40:31 +0100

    run predictions

--- mainly_about_gpt.py
+++ mainly_about_gpt.py
@@ -66,38 +66,55 @@ if __name__ == '__main__':
     #     with open(f'/n/home09/esilcock/mentions_benefits/mentions_benefits_{year}.json') as f:
     #         mentions_benefits = json.load(f)
 
-    with open('Labelled_data/mainly_about/final_merged_tricky_resolved.json') as f:
-        dat = json.load(f)
+    # with open('Labelled_data/mainly_about/final_merged_tricky_resolved.json') as f:
+    #     dat = json.load(f)
 
-    ######
-    dat = dat[:5]
-    ######
+    # # Summarise with GPT
+    # client = OpenAI(api_key="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")
+
+    # for art in tqdm(dat):
+    #     sum = generate_summary(client, art['data']['headline'], art['data']['article'])
+    #     art['data']['summary'] = sum
+
+    # with open('Labelled_data/mainly_about/final_merged_with_summaries.json', 'w') as f:
+    #     json.dump(dat, f)
+    with open('Labelled_data/mainly_about/final_merged_with_summaries.json') as f:
+        dat = json.load(f)
 
     gts = [art['annotations'][0]['result'][0]['value']['choices'][0] for art in dat]
 
-    # Summarise with GPT
-    client = OpenAI(api_key="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")
 
-    for art in tqdm(dat):
-        sum = generate_summary(client, art['data']['headline'], art['data']['article'])
-        art['data']['summary'] = sum
+    # Predict over summaries
+    base_model='roberta-large'
+    tokenized_data = tokenize(dat, tokenization_model=base_model, max_token_length=512)
 
-    with open('Labelled_data/mainly_about/final_merged_with_summaries.json', 'w') as f:
-        json.dump(dat, f)
+    # Run inference
+    preds = predict(
+        tokenized_data,
+        finetuned_topic_model='/n/home09/esilcock/NIHDAL/trained_models/kw_initialisation/full_dat_16_5e-06_v2/checkpoint-840',
+        batch_size=512
+    )
 
-    # # Predict over summaries
-    # base_model='roberta-large'
-    # tokenized_data = tokenize(dat, tokenization_model=base_model, max_token_length=512)
+    # Eval
+    tps = 0
+    tns = 0
+    fps = 0
+    fns = 0
 
-    # # Run inference
-    # preds = predict(
-    #     tokenized_data,
-    #     finetuned_topic_model='/n/home09/esilcock/NIHDAL/trained_models/kw_initialisation/full_dat_16_5e-06_v2/checkpoint-840',
-    #     batch_size=512
-    # )
+    for i, pred in preds:
+        if pred == 1 and gts[i] == "Mainly about benefits":
+            tps +=1
+        elif pred == 1 and gts[i] == "Not mainly about benefits":
+            fps += 1
+        if pred == 0 and gts[i] == "Mainly about benefits":
+            fns +=1
+        elif pred == 1 and gts[i] == "Not mainly about benefits":
+            tns += 1
 
-    # print(preds)
-    # print(gts)
-
-
-
+    print(tps, tns, fps, fns)
+    
+    recall = tps/(tps + fns)
+    precision = tps/(tps + fps)
+    f1 = recall * precision * 2 /(recall + precision)
+  
+    print(f1, precision, recall)
