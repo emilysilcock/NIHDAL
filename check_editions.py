1fd2c582e18394d2383c9c90364e5a9492b45596
emilysilcock <emilysilcock@gmail.com>
2024-06-18 14:15:30 +0100

    initial commit

---
+++ check_editions.py
@@ -0,0 +1,17 @@
+import json
+from tqdm import tqdm
+
+editions = []
+
+for year in range(2013, 2023):
+
+    print(f"******************{year}**********************")
+
+    with open(f'/n/home09/esilcock/mentions_benefits/mentions_benefits_{year}.json') as f:
+        mentions_benefits = json.load(f)
+
+    for art in tqdm(mentions_benefits):
+        if art['edition'] not in editions:
+            editions.append(art['edition'])
+
+print(editions)
