89455e998eb89863f470a38290f324deede8e7b0
emilysilcock <emilysilcock@gmail.com>
2024-07-05 14:18:52 -0400

    test

--- pull_non_national_to_check.py
+++ pull_non_national_to_check.py
@@ -6,8 +6,8 @@ for year in range(2013, 2023):
     print(f"******************{year}**********************")
 
 
-    with open(f'/n/home09/esilcock/mainly_about_benefits/mainly_about_benefits_{year}.json') as f:
-        national = json.load(f)
+    # with open(f'/n/home09/esilcock/mainly_about_benefits/mainly_about_benefits_{year}.json') as f:
+    #     national = json.load(f)
 
     with open(f'/n/home09/esilcock/mainly_about_benefits/mainly_about_benefits_{year}_non_national.json') as f:
         non_national = json.load(f)
