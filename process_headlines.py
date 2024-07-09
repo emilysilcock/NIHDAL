7e953648fe30577f8d4162f52cc02848ceffa529
emilysilcock <emilysilcock@gmail.com>
2024-07-09 14:54:14 -0400

    test

--- process_headlines.py
+++ process_headlines.py
@@ -1,29 +1,31 @@
 import json
 from tqdm import tqdm
 
-date_lists = {}
-for year in range(2013, 2023):
+# date_lists = {}
+# for year in range(2013, 2023):
 
-    print(f'*******{year}*******')
+#     print(f'*******{year}*******')
 
     
-    with open(f'/n/home09/esilcock/clean_Sun_data/{year}_cleaned.json') as f:
-        national = json.load(f)
+#     with open(f'/n/home09/esilcock/clean_Sun_data/{year}_cleaned.json') as f:
+#         national = json.load(f)
 
-    with open(f'/n/home09/esilcock/clean_Sun_data/{year}_cleaned_non_national.json') as f:
-        non_national = json.load(f)
+#     with open(f'/n/home09/esilcock/clean_Sun_data/{year}_cleaned_non_national.json') as f:
+#         non_national = json.load(f)
 
-    all_dat = national + non_national
+#     all_dat = national + non_national
 
-    for art in tqdm(all_dat):
+#     for art in tqdm(all_dat):
 
-        if art['date'] not in date_lists:
-            date_lists[art['date']] = []
+#         if art['date'] not in date_lists:
+#             date_lists[art['date']] = []
 
-        date_lists[art['date']].append({'headline': art["headline"], 'edition': art["edition"]})
+#         date_lists[art['date']].append({'headline': art["headline"], 'edition': art["edition"]})
 
-with open('all_headlines.json', 'w') as f:
-    json.dump(date_lists, f, indent=4)
+# with open('all_headlines.json', 'w') as f:
+#     json.dump(date_lists, f, indent=4)
+with open('all_headlines.json') as f:
+    date_lists = json.load(f)
 
 
 print("Finding duplicates")
