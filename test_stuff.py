6db5d0d7bca0f1c4fafd3090520f988a591ead7f
simon-lowe <57216897+simon-lowe@users.noreply.github.com>
2025-04-19 14:18:45 -0400

    test cor

--- test_stuff.py
+++ test_stuff.py
@@ -1,4 +1,4 @@
 import torch
 print(f'PyTorch version: {torch.__version__}')
 print(f'CUDA available: {torch.cuda.is_available()}')
-print(f'CUDA version: {torch.version.cuda}' if torch.cuda.is_available() else 'CUDA not available')"
+print(f'CUDA version: {torch.version.cuda}' if torch.cuda.is_available() else 'CUDA not available')
