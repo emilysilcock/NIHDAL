7bc690844bf586887174724ce9c7077baa54bccf
emilysilcock <emilysilcock@gmail.com>
2025-04-28 10:46:31 -0400

    debug

--- active_learning_inference_france.py
+++ active_learning_inference_france.py
@@ -373,13 +373,16 @@ if __name__ == '__main__':
                 labels.append(1)
                 all_labels.append(1)
 
-        else:
             check.append(article['ln_id'])
+
+        else:
             all_labels.append(small_text.base.LABEL_UNLABELED)
 
         # Create pool
         text = str(article['headline']) + sep + str(article['article'])
         texts.append(text)
+
+    print([i['ln_id'] for i in parsed_labelled_data if i['ln_id'] not in check])
 
     print(f"Pool size: {len(texts)}")
     print(f"of which {len(labels)} are labelled")
