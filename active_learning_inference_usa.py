57839c4372a5a16965c91b4c3f013c4ec00ba0cb
emilysilcock <emilysilcock@gmail.com>
2025-05-20 08:42:49 +0100

    Next sample

--- active_learning_inference_usa.py
+++ active_learning_inference_usa.py
@@ -340,7 +340,8 @@ if __name__ == '__main__':
     parsed_labelled_data = open_labelled_data([
         'Labelled_data/kw_initialisation/full_corrected.json',
         'Labelled_data/usa_mentions/usa_sample_1.json',
-        'Labelled_data/usa_mentions/usa_sample_2.json' 
+        'Labelled_data/usa_mentions/usa_sample_2.json',
+        'Labelled_data/usa_mentions/usa_sample_3.json'
         ])
 
     texts = []
@@ -408,5 +409,5 @@ if __name__ == '__main__':
             "data": sample_list[i]
         })
 
-    with open(f'data_to_label/usa_sample_3.json', 'w') as f:
+    with open(f'data_to_label/usa_sample_4.json', 'w') as f:
         json.dump(to_label, f, indent=2)
