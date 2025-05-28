fa91480f833f2095daeed77361495b0df2a05d0f
emilysilcock <emilysilcock@gmail.com>
2025-05-28 22:12:43 +0200

    Add sample

--- active_learning_inference_usa.py
+++ active_learning_inference_usa.py
@@ -341,7 +341,8 @@ if __name__ == '__main__':
         'Labelled_data/kw_initialisation/full_corrected.json',
         'Labelled_data/usa_mentions/usa_sample_1.json',
         'Labelled_data/usa_mentions/usa_sample_2.json',
-        'Labelled_data/usa_mentions/usa_sample_3.json'
+        'Labelled_data/usa_mentions/usa_sample_3.json',
+        'Labelled_data/usa_mentions/usa_sample_4.json'
         ])
 
     texts = []
@@ -409,5 +410,5 @@ if __name__ == '__main__':
             "data": sample_list[i]
         })
 
-    with open(f'data_to_label/usa_sample_4.json', 'w') as f:
+    with open(f'data_to_label/usa_sample_5.json', 'w') as f:
         json.dump(to_label, f, indent=2)
