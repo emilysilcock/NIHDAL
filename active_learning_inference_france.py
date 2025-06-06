50df3a6bbd8364d0903f91de0535f22cfcebf3f0
emilysilcock <emilysilcock@gmail.com>
2025-06-06 09:04:57 +0200

    Next sample

--- active_learning_inference_france.py
+++ active_learning_inference_france.py
@@ -347,7 +347,8 @@ if __name__ == '__main__':
         )
 
     parsed_labelled_data = open_labelled_data(
-        ['Labelled_data/french_mentions/france_sample_1.json'],
+        ['Labelled_data/french_mentions/france_sample_1.json', 
+         'Labelled_data/french_mentions/france_sample_2.json',],
         already_cleaned = 'Labelled_data/other_languages/French_mentions_benefits.json')
 
     texts = []
@@ -415,5 +416,5 @@ if __name__ == '__main__':
             "data": sample_list[i]
         })
 
-    with open(f'data_to_label/france_sample_2.json', 'w') as f:
+    with open(f'data_to_label/france_sample_3.json', 'w') as f:
         json.dump(to_label, f, indent=2, ensure_ascii=False)
