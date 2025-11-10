a3e60eafeb3a4d2e8ead7a80c92cf69cd750d3c2
emilysilcock <emilysilcock@gmail.com>
2025-11-09 20:05:41 -0500

    Next round france

--- active_learning_inference_france.py
+++ active_learning_inference_france.py
@@ -347,7 +347,8 @@ if __name__ == '__main__':
         )
 
     parsed_labelled_data = open_labelled_data(
-        ['/n/home09/esilcock/stigma-non-take-up/0_data/raw_data/newspapers/Labelled_data/french_mentions/france_sample_corrected.json'
+        ['/n/home09/esilcock/stigma-non-take-up/0_data/raw_data/newspapers/Labelled_data/french_mentions/france_sample_corrected.json',
+        '/n/home09/esilcock/stigma-non-take-up/0_data/raw_data/newspapers/Labelled_data/french_mentions/france_sample_6.json'
         ],
         already_cleaned = 'Labelled_data/other_languages/French_mentions_benefits.json')
 
